3b7547d9a5adb56f3bd2cc8abe1b65d9a772cf4f
nolensprotocol <nolensprotocol@gmail.com>
2025-04-04 06:04:28 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
 
       <section className="py-24 bg-white text-gray-900">
         <div className="max-w-6xl mx-auto px-6 text-center">
-          <h2 className="text-3xl font-bold mb-10">Token Utility</h2>
+          <h2 className="text-3xl font-bold mb-10">Where to Use?</h2>
           <div className="grid gap-8 grid-cols-1 md:grid-cols-3">
             <div className="transition duration-700 ease-out bg-gray-100 p-6 rounded-xl shadow hover:shadow-lg hover:scale-105 hover:bg-white">
               <h3 className="font-semibold text-lg mb-2">Access & Payments</h3>
