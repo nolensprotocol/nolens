b0670d7bbb80089acd6199b82c2bd1af37c10136
nolensprotocol <nolensprotocol@gmail.com>
2025-04-04 04:21:00 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm py-4 px-6 flex items-center justify-between w-full">
         <div className="flex items-center space-x-1.0">
-          <img src="/nolens_icon.png" alt="Nolens Logo" className="w-6 h-6 mt-1.3" />
+          <img src="/nolens_icon.png" alt="Nolens Logo" className="w-6 h-6 mt-2" />
           <span className="text-gray-900 text-lg font-sans tracking-wide lowercase font-sans">nolens</span>
         </div>
         <nav className="hidden md:flex items-center space-x-6 text-sm text-gray-800 font-medium">
