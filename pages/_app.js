87eda16c668cc8c300b4f7a6680fca48912aa22c
nolensprotocol <nolensprotocol@gmail.com>
2025-04-06 19:13:44 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import { WagmiProvider, configureChains, createConfig } from 'wagmi';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { polygonMumbai } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -19,10 +19,9 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiProvider config={config}>
+      <WagmiConfig config={config}>
         <Component {...pageProps} />
-      </WagmiProvider>
+      </WagmiConfig>
     </QueryClientProvider>
   );
 }
-
