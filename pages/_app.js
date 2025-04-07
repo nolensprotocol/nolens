aa1cadf2bfdfc33cb24c62e269afb70a64a195b7
nolensprotocol <nolensprotocol@gmail.com>
2025-04-07 09:27:11 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,34 @@
-// pages/_app.js
-import '../styles/globals.css';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { polygonMumbai } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider } from 'next-themes';
-import Navbar from '../components/Navbar';
+import '../styles/globals.css'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { polygonMumbai } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
+import Navbar from '../components/Navbar'
 
 const { chains, publicClient } = configureChains(
   [polygonMumbai],
   [publicProvider()]
-);
+)
 
 const config = createConfig({
-  autoConnect: true,
+  autoConnect: true, // ✅ Reconnect wallet on page refresh
   publicClient,
-});
+})
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <QueryClientProvider client={queryClient}>
         <WagmiConfig config={config}>
-          {/* Navbar added globally */}
+          {/* Global site header */}
           <Navbar />
           <Component {...pageProps} />
         </WagmiConfig>
       </QueryClientProvider>
     </ThemeProvider>
-  );
+  )
 }
+
