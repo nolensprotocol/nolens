847708c252b395335242dafd4fa38261d9be0f43
nolensprotocol <nolensprotocol@gmail.com>
2025-04-08 20:38:15 +0900

    Update DocsBackLink.js

--- components/DocsBackLink.js
+++ components/DocsBackLink.js
@@ -1,12 +1,15 @@
-// components/DocsBackLink.js
 import Link from 'next/link'
 
 export default function DocsBackLink() {
   return (
-    <div className="mt-20 pt-8 border-t border-gray-200 text-sm">
-      <Link href="/docs" className="text-gray-600 hover:text-gray-900 transition">
-        ← Back to Docs
+    <div className="mt-20 pt-8 border-t border-gray-200 flex justify-center">
+      <Link
+        href="/docs"
+        className="text-gray-600 hover:text-gray-900 transition text-sm"
+      >
+        ← Back
       </Link>
     </div>
   )
 }
+
