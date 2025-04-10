c5a52afd6cc7768e434c9cdfad3a7f94a6924ebb
nolensprotocol <nolensprotocol@gmail.com>
2025-04-10 12:50:18 +0900

    Add files via upload

---
+++ pages/_app_250410.js
@@ -0,0 +1,34 @@
+import '../styles/globals.css'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { polygonMumbai } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
+import Navbar from '../components/Navbar'
+
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [publicProvider()]
+)
+
+const config = createConfig({
+  autoConnect: true, // ✅ Reconnect wallet on page refresh
+  publicClient,
+})
+
+const queryClient = new QueryClient()
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <QueryClientProvider client={queryClient}>
+        <WagmiConfig config={config}>
+          {/* Global site header */}
+          <Navbar />
+          <Component {...pageProps} />
+        </WagmiConfig>
+      </QueryClientProvider>
+    </ThemeProvider>
+  )
+}
+
